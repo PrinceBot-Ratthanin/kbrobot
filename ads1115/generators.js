a6cbb6aa3fc21c6b067faa7f91c5146e0fb63f25
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2020-01-10 02:07:09 +0700

    Update generators.js

--- ads1115/generators.js
+++ ads1115/generators.js
@@ -1,4 +1,4 @@
 Blockly.JavaScript['ads1115.get_volt'] = function(block) {
-  var code = 'DEV_I2C0.ADS1115(0,73).get_volt(' + block.getFieldValue('AIN') + ') * 310.303\n';
+  var code = 'DEV_I2C0.ADS1115(0,73).get_volt(' + block.getFieldValue('AIN') + ') *500\n';
   return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
